8e57ac0454a37e22c430075c6b30b0a71ee35853
thegamerofkingsik <112348802+thegamerofkingsik@users.noreply.github.com>
2026-02-20 20:25:12 +0100

    Auto-sync: the script is give me this error  could not find f...

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,12 @@ fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 game 'gta5'
-name 'ox_inventory'
-author 'Overextended'
+
+author 'SwisserAI'
+description 'Generated with SwisserAI - https://ai.swisser.dev'
 version '2.44.1'
+
 repository 'https://github.com/overextended/ox_inventory'
-description 'Slot-based inventory with item metadata support'
 
 dependencies {
     '/server:6116',
